dfcb0cafe6d74a2992bd6c0473f1a04b4851e9f8
karan <ikaranmali@gmail.com>
2022-05-04 08:04:49 +0530

    updated sql query

--- elib-sql-schema.sql
+++ elib-sql-schema.sql
@@ -1,25 +1,22 @@
-
 create database elib;
-
 use elib;
-
 create table borrow(
 	id  int primary key auto_increment,
 	S_Name char(150),
 	B_title varchar(150),
-	B_Edition varchar(150),
-	B_Publication varchar(150),
 	date timestamp default CURRENT_TIMESTAMP,
 	token_no varchar(15),
+	B_Ed varchar(150),
+    B_Pub varchar(150),
 	due varchar(12));
-
+    
 create table b_return(
 	id  int primary key auto_increment,
 	St_Name char(150),
 	Bo_title varchar(150),
 	date timestamp default CURRENT_TIMESTAMP,
 	token_no varchar(15),
-	due_date varchar(12),
+    due_date varchar(12),
 	charges int);
     
 create table students(
@@ -34,7 +31,5 @@ create table books(
 	id int primary key auto_increment,
 	Title varchar(150),
 	Edition varchar(150),
-	Publication varchar(150),
+    Publication varchar(150),
 	Author varchar(50));
-
-show databases;
